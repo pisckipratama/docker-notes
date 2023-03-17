4c0449794b9ef18744222beedef3d15d6049d447
Piscki Pratama <pisckipratama@gmail.com>
2023-03-17 23:30:48 +0700

    add project 5 and edit Dockerfile add multiple files

---
+++ 5-docker-compose/client/src/App.js
@@ -0,0 +1,21 @@
+import "./App.css";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import Fib from "./Fib";
+import OtherPage from "./OtherPage";
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <Link to="/">Home | </Link>
+        <Link to="/otherpage">Other Page</Link>
+        <div>
+          <Route exact path="/" component={Fib} />
+          <Route path="/otherpage" component={OtherPage} />
+        </div>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
